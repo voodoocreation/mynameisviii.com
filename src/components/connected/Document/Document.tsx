aa5209fc3a9b46cdd30cf07cce05467cc091c2d0
Raice Hannay <voodoocreation@gmail.com>
2018-10-24 16:13:26 +1300

    update snapshots

--- src/components/connected/Document/Document.tsx
+++ src/components/connected/Document/Document.tsx
@@ -27,10 +27,28 @@ const Meta: React.SFC<{}> = () =>
 
       <link rel="manifest" href="/static/manifest.json" />
 
-      <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
-      <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#540008" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicon/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="/static/favicon/safari-pinned-tab.svg"
+        color="#540008"
+      />
       <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#540008" />
       <meta name="theme-color" content="#ffeaea" />
