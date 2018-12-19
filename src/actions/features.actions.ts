8c41df1e51b99f00fe7982ed42b92f3a7fc8f56c
Raice Hannay <voodoocreation@gmail.com>
2018-12-19 19:57:17 +1300

    refactor features.sagas

--- src/actions/features.actions.ts
+++ src/actions/features.actions.ts
@@ -2,12 +2,6 @@ import actionCreatorFactory from "typescript-fsa";
 
 const actionCreator = actionCreatorFactory("FEATURES");
 
-export const addFeature = actionCreator<PLAddFeature>("ADD_ONE");
+export const addFeatures = actionCreator<PLAddFeatures>("ADD");
 
-export const addFeatures = actionCreator<PLAddFeatures>("ADD_MULTIPLE");
-
-export const removeFeature = actionCreator<PLRemoveFeature>("REMOVE_ONE");
-
-export const removeFeatures = actionCreator<PLRemoveFeatures>(
-  "REMOVE_MULTIPLE"
-);
+export const removeFeatures = actionCreator<PLRemoveFeatures>("REMOVE");
--- src/sagas/features.sagas.ts
+++ src/sagas/features.sagas.ts
@@ -4,47 +4,37 @@ import * as actions from "../actions/root.actions";
 
 export const addFeaturesSaga = (ports: IStorePorts) =>
   function*() {
-    yield takeLatest(
-      [actions.addFeature, actions.addFeatures],
-      function*(action: {
-        type: string;
-        payload: PLAddFeature | PLAddFeatures;
-      }) {
-        const featuresToAdd =
-          typeof action.payload === "string"
-            ? [action.payload]
-            : action.payload;
+    yield takeLatest(actions.addFeatures, function*(action: {
+      type: string;
+      payload: PLAddFeatures;
+    }) {
+      const featuresToAdd =
+        typeof action.payload === "string" ? [action.payload] : action.payload;
 
-        yield call(
-          ports.features.push.bind(
-            ports.features,
-            ...featuresToAdd.filter(item => !ports.features.includes(item))
-          )
-        );
-      }
-    );
+      yield call(
+        ports.features.push.bind(
+          ports.features,
+          ...featuresToAdd.filter(item => !ports.features.includes(item))
+        )
+      );
+    });
   };
 
 export const removeFeaturesSaga = (ports: IStorePorts) =>
   function*() {
-    yield takeLatest(
-      [actions.removeFeature, actions.removeFeatures],
-      function*(action: {
-        type: string;
-        payload: PLRemoveFeature | PLRemoveFeatures;
-      }) {
-        const featuresToRemove =
-          typeof action.payload === "string"
-            ? [action.payload]
-            : action.payload;
+    yield takeLatest(actions.removeFeatures, function*(action: {
+      type: string;
+      payload: PLRemoveFeatures;
+    }) {
+      const featuresToRemove =
+        typeof action.payload === "string" ? [action.payload] : action.payload;
 
-        for (const feature of featuresToRemove) {
-          yield call(
-            ports.features.splice.bind(ports.features),
-            ports.features.indexOf(feature),
-            1
-          );
-        }
+      for (const feature of featuresToRemove) {
+        yield call(
+          ports.features.splice.bind(ports.features),
+          ports.features.indexOf(feature),
+          1
+        );
       }
-    );
+    });
   };
